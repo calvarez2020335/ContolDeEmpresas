974d4555af82639a264e3979f74ccf259d64dcb2
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-03-02 09:02:09 -0600

    Entrega Final

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -2,6 +2,11 @@ const Empleados = require("../models/empleados.model");
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const doc = new PDFDocument();
+let date = new Date()
+let day = date.getDate()
+let month = date.getMonth() + 1
+let year = date.getFullYear()
+
 
 function agregarEmpleado(req, res) {
   const parametros = req.body;
@@ -189,18 +194,75 @@ function buscarTodosLosEmpleados(req, res){
 }
 
 function pdf(req, res){
-  Empleados.find({idEmpresa: req.user.sub}, (err, empleadoEncontrado)=>{
-    for(let i = 0; i < empleadoEncontrado.length; i++) {
-      doc.pipe(fs.createWriteStream("reportes/"+ req.user.nombre + ".pdf"));
-      
-      doc.text(empleadoEncontrado[i]._id)
-      doc.text(empleadoEncontrado[i].nombre + " " + empleadoEncontrado[i].apellido);
-      doc.text(empleadoEncontrado[i].puesto)
+  doc.pipe(fs.createWriteStream("reportes/" + req.user.nombre + ".pdf"));
+  doc
+    .fillColor("#444444")
+    .fontSize(32)
+    .text("Reporte de Empleados", {
+      align: "center",
+      bold: true
+    });
+  doc
+    .fillColor("#444444")
+    .fontSize(32)
+    .text(req.user.nombre, { align: "center", bold: true, underline: true })
+    .moveDown();
+  
+  Empleados.find({ idEmpresa: req.user.sub }, (err, empleadoEncontrado) => {
+    ////////////////////////////////////////////////////////////////
+    for (let i = 0; i < empleadoEncontrado.length; i++) {
+      empleadoEncontrado[i].nombre;
+  }
+    const beginningOfPage = 50;
+    const endOfPage = 500;
+
+    doc.moveTo(beginningOfPage, 200).lineTo(endOfPage, 200).stroke();
+
+    doc.fontSize(16).text(`Total de empleados: ` +empleadoEncontrado.length, 50, 220);
+
+    doc.moveTo(beginningOfPage, 250).lineTo(endOfPage, 250).stroke().moveDown();
+    /////////////////////////////////////////////////////////////////
+
+    const tableTop = 270;
+    const itemCodeX = 50;
+    const descriptionX = 160;
+    const quantityX = 260;
+    const priceX = 400;
+    
+    doc
+      .fontSize(15)
+      .text("Nombre", itemCodeX, tableTop, { bold: true, underline: true })
+      .text("Apellido", descriptionX, tableTop, {bold: true, underline: true})
+      .text("Puesto", quantityX, tableTop, { bold: true, underline: true})
+      .text("Departamento", priceX, tableTop, { bold: true, underline: true});
+
+      let i = 0;
+    for (i = 0; i < empleadoEncontrado.length; i++) {
+
+      const y = tableTop + 25 + (i*25)
+
+      doc.text(empleadoEncontrado[i].nombre, itemCodeX, y);
+      doc.text(empleadoEncontrado[i].apellido, descriptionX,y);
+      doc.text(empleadoEncontrado[i].puesto, quantityX, y);
+      doc.text(empleadoEncontrado[i].departamento, priceX, y)
+
     }
+
+    doc
+    .fontSize(10)
+    if (month < 10) {
+      doc.text(`${day}-0${month}-${year}`, 50, 700, { align: "center" });
+    } else {
+      doc.text(`${day}-${month}-${year}`, 50, 700, { align: "center" });
+    }
+
+
     doc.end();
-  })
+  });
   return res.status(200).send("Pdf generado en la carpeta reportes");
 }
+
+
 
 
 module.exports = {
